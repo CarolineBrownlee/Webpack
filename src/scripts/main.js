0eb6cbc8395e7ba2edf0654be4b235649d6791a6
Caroline Brownlee <brownleecaroline@gmail.com>
2019-11-05 23:13:52 -0600

    ready to start practice exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,14 @@
-const message = "Your Webpack application is set up and ready to go. Please start writing code."
+// Practice: Contact List
 
-document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
+// Build a Webpack application with the following four JS components:
 
+// A ContactCollection component that loads existing contacts from a json-server API, and saves new ones.
+// A Contact component that displays a person's name, phone number, and address.
+// A ContactList component that displays all contacts. It should import the Contact component and the ContactCollection component.
+// A ContactForm component that listens for when the submit button is pressed. When it is triggered, a new contact should be POSTed to the API. It should import the ContactCollection component.
+// In main.js, import the ContactList component and the ContactForm component.
 
+// The user should see the contact form at the top of the view, and the list of contacts underneath it.
+
+// Food for thought: Are there any other modules that could be made? Do any modules have more than one possible responsibility? Perhaps something that is a general utility function.
 
